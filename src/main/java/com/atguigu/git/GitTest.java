9bd33819ad37ca0697fad60b8491af59ddf82abc
raster1 <105350463+raster1@users.noreply.github.com>
2022-05-12 20:59:02 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("hello git444");
         System.out.println("got-fix test");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
